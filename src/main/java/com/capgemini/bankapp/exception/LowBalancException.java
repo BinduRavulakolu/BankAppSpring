76e0dd2d3a0a52cea6caa4a988665c1d68f07b4f
Ravulakolu <bindu.ravulakolu@capgemini.com>
2018-09-22 01:22:34 -0400

    first commit

---
+++ src/main/java/com/capgemini/bankapp/exception/LowBalancException.java
@@ -0,0 +1,9 @@
+package com.capgemini.bankapp.exception;
+
+public class LowBalancException extends Exception {
+
+	public LowBalancException(String string) {
+super.getMessage();
+	}
+
+}
---
+++ src/main/java/com/capgemini/bankapp/repository/BankAccountRepository.java
@@ -0,0 +1,14 @@
+package com.capgemini.bankapp.repository;
+
+
+
+import java.util.List;
+
+import com.capgemini.bankapp.entities.BankAccount;
+
+public interface BankAccountRepository {
+	public double getBalance(long accountId);
+	public boolean updateBalance(long accountId, double newBalance);
+	
+
+}
